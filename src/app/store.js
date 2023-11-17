b3722adca529d4ca41134c0eeed90db5f991f0bc
ashish.chodankar <ashish.chodankar@squareyards.co.in>
2023-11-17 15:17:11 +0530

    middleware comment changed

--- src/app/store.js
+++ src/app/store.js
@@ -14,7 +14,7 @@ const store = configureStore({
     iceCream: iceCreamReducer,
     user: userReducer,
   },
-  // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger)
+  // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 export default store;
